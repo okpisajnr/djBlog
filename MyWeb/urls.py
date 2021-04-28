f64ef2394f7b716229db95c586c473a685261f08
okpisajnr <okpisamichael@gmail.com>
2021-04-28 16:31:02 +0100

    Initial commit

--- MyWeb/urls.py
+++ MyWeb/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
  
-    #path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),name="password_reset"),
-    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name="password_reset_done"),
-    #path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name="password_reset_confirm"),
-    #path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name="password_reset_complete"),
-]
+    ]
